fe7362721e4bc74038e2e1cbb99aad5f8162b999
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2025-04-30 09:20:29 +0100

    Modifying bump logic
    Token worked but do i really want to bypass things on actions?
    This opens up potentially the ability to bypass all checks and get stuff through
    
    [Ticket: #192]

--- .ci/bump.sh
+++ .ci/bump.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
 
 # this is not to be run, this is just for testing.
-
+VERSION=''
 RELEASE_TYPE="false"
+#(git tag --list "0.0.*"  --sort=-version:refname | head -n 1) |python ./.ci/getversion.py $RELEASE_TYPE 2>&1 > $VERSION
+#version=$((git tag --list "0.0.*"  --sort=-version:refname | head -n 1) |python ./.ci/getversion.py $RELEASE_TYPE 2>&1)
 
-version=$(python ./.ci/getversion.py $RELEASE_TYPE 2>&1)
-echo $version
+
+VERSION=$(git tag --list "0.0.*"  --sort=-version:refname | head -n 1 | python ./.ci/bump_v2.py 2>&1)
+
+
+echo $VERSION
 
---
+++ .ci/bump_v2.py
@@ -0,0 +1,49 @@
+# Version 2 of the bump.
+# This does not need to push to source....
+import sys
+import json
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+def open_ci():
+    with open(f'{path}/ci_2.json') as json_data:
+        ci_file = json.load(json_data)
+        json_data.close()
+        return ci_file
+    
+def bump_major(ci_file, version):
+    if ci_file["version_config"]["major"] > int(version[0]):
+        version[0] = int(version[0]) + 1
+        print(f'{version[0]}.0.0')
+    else:
+        print('ERROR: Major version must be more than the current repository semver.')
+        print('')
+        exit(1)
+
+def bump_minor(version):
+    version[1] = int(version[1]) + 1
+    print(f'{version[0]}.{version[1]}.0')
+
+def bump_revision(version):
+    version[2] = int(version[2]) + 1
+    print(f'{version[0]}.{version[1]}.{version[2]}')
+
+def main():
+    version = sys.stdin.read().rstrip().split('.')
+    ci_file = open_ci()
+
+    match ci_file["version_config"]["bump"]:
+        case 'major':
+            bump_major(ci_file, version)
+        case 'minor':
+            bump_minor(version)
+        case 'rev':
+            bump_revision(version)
+        case _:
+            bump_revision(version)
+    
+
+
+if __name__ == '__main__':
+    main()
